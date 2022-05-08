f6d9689480bf9b77f654244d37a14d2891c66157
Thomas Kruse <tk@trion.de>
2022-05-08 23:13:25 +0200

    init

---
+++ 00-basic-spring/src/main/java/de/trion/training/common/training/TrainingRepository.java
@@ -0,0 +1,13 @@
+package de.trion.training.common.training;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TrainingRepository extends JpaRepository<Training, Integer> {
+
+   //@Query(value = "SELECT COL_LOCATION FROM TBL_TRAININGS",nativeQuery = true)
+   @Query("SELECT t.location FROM Training t")
+   List<String> findLocations();
+}
