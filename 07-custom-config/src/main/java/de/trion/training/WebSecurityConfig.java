17c48d6dad8cd026e79adf05baa4e311bcb63563
Thomas Kruse <tk@trion.de>
2022-05-14 00:46:25 +0200

    wip

---
+++ 07-custom-config/src/main/java/de/trion/training/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package de.trion.training;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity(debug = true)
+@Configuration(proxyBeanMethods = false)
+public class WebSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+           .username("user")
+           .password("password")
+           .roles("USER")
+           .build();
+
+        var admin = User.builder()
+            .username("admin")
+            .password("admin")
+            .roles("ADMIN")
+            .passwordEncoder(s -> "{noop}"+s)
+            .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+}
