c564f230492f4a1af7c502333d3a7d86e554879b
Thomas Kruse <tk@trion.de>
2022-05-14 21:06:34 +0200

    config

--- 07-custom-config/src/main/java/de/trion/training/WebSecurityConfig.java
+++ 07-custom-config/src/main/java/de/trion/training/WebSecurityConfig.java
@@ -2,10 +2,17 @@ package de.trion.training;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @EnableWebSecurity(debug = true)
 @Configuration(proxyBeanMethods = false)
@@ -26,6 +33,47 @@ public class WebSecurityConfig {
             .build();
 
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Profile("dev")
+    @Bean
+    public SecurityFilterChain h2Filter(HttpSecurity httpSecurity) throws Exception
+    {
+        httpSecurity
+           .antMatcher("/h2-console/**")
+           .authorizeRequests()
+               .anyRequest()
+               .permitAll()
+           .and()
+           .csrf().disable()
+           .headers()
+           .frameOptions().disable();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public SecurityFilterChain trainings(HttpSecurity httpSecurity) throws Exception
+    {
+        httpSecurity
+           .mvcMatcher("/trainings/**")
+           .authorizeRequests()
+           .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+           .mvcMatchers("/trainings/*/edit").hasRole("ADMIN")
+           .anyRequest().permitAll();
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public SecurityFilterChain login(HttpSecurity httpSecurity) throws Exception
+    {
+//        var anon = new User("anonymousUser", "", List.of(new SimpleGrantedAuthority("ANONYMOUS")) );
+//        httpSecurity.anonymous().principal(anon);
+
+        httpSecurity
+           .formLogin()
+           .and().logout().logoutSuccessUrl("/");
+        return httpSecurity.build();
     }
 
 }
--- 07-custom-config/src/main/java/de/trion/training/web/InfoController.java
+++ 07-custom-config/src/main/java/de/trion/training/web/InfoController.java
@@ -1,11 +1,14 @@
 package de.trion.training.web;
 
+import de.trion.training.common.training.TrainingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
@@ -35,14 +38,31 @@ public class InfoController {
       return new ModelAndView("info", Map.of("heading", "Info!"));
    }
 
-   @RequestMapping("/info2")
-   public ModelAndView info2(Principal principal, Authentication authentication) {
+   @Autowired
+   TrainingService trainingService;
 
-      logger.info("Name: {}, principal: {}, authorities: {}", authentication.getName(), principal, authentication.getAuthorities());
-      var details = authentication.getDetails();
-      if (details instanceof WebAuthenticationDetails webDetails) {
-         logger.info("Remote ip: {}, session: {}", webDetails.getRemoteAddress(), webDetails.getSessionId());
+   @RequestMapping("/info2")
+   public ModelAndView info2(Principal principal, Authentication authentication,
+                             @AuthenticationPrincipal UserDetails user, @CurrentSecurityContext SecurityContext context) {
+
+      logger.info("principal: {}", principal);
+      if (authentication != null)
+      {
+         logger.info("Name: {}, authorities: {}", authentication.getName(), authentication.getAuthorities());
+         var details = authentication.getDetails();
+         if (details instanceof WebAuthenticationDetails webDetails)
+         {
+            logger.info("Remote ip: {}, session: {}", webDetails.getRemoteAddress(), webDetails.getSessionId());
+         }
       }
+      else
+      {
+         logger.info("Authentication is null.");
+      }
+
+      logger.info("user: {}", user);
+      var contextAuthentication = context.getAuthentication();
+      logger.info("context principal: {} ({}), context authentication: {}", principal, contextAuthentication.getPrincipal().getClass(), contextAuthentication);
 
       return new ModelAndView("info", Map.of("heading", "Info!"));
    }
