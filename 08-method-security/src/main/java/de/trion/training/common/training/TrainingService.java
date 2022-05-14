8253cf7eb06074720357722f5fd4b686fdc2eda7
Thomas Kruse <tk@trion.de>
2022-05-14 23:19:03 +0200

    method security

--- 08-method-security/src/main/java/de/trion/training/common/training/TrainingService.java
+++ 08-method-security/src/main/java/de/trion/training/common/training/TrainingService.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Service;
