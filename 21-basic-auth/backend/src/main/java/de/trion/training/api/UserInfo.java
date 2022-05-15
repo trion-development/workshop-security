f5bb19c68e89627e933f9401813f5948c2737213
Thomas Kruse <tk@trion.de>
2022-05-15 23:49:53 +0200

    userinfo

--- 21-basic-auth/backend/src/main/java/de/trion/training/api/UserInfo.java
+++ 21-basic-auth/backend/src/main/java/de/trion/training/api/UserInfo.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(allowCredentials = "true", originPatterns = "*")
 @RequestMapping("/api/userinfo")
 public class UserInfo
 {
