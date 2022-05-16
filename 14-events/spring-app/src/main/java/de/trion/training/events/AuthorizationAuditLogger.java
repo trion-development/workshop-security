5605bc1c70f787a7999c8bcdbfdc8202dec0c956
Thomas Kruse <tk@trion.de>
2022-05-17 00:41:01 +0200

    update

---
+++ 14-events/spring-app/src/main/java/de/trion/training/events/AuthorizationAuditLogger.java
@@ -0,0 +1,20 @@
+package de.trion.training.events;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.security.AbstractAuthorizationAuditListener;
+import org.springframework.security.access.event.AbstractAuthorizationEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorizationAuditLogger extends AbstractAuthorizationAuditListener
+{
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void onApplicationEvent(AbstractAuthorizationEvent event)
+    {
+        // siehe auch org.springframework.boot.actuate.security.AuthenticationAuditListener
+        logger.warn("AuthZEvent {} ({}) ", event, event.getClass().getSimpleName());
+    }
+}
